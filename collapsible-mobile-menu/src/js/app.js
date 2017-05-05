1452531b86b0e158e699acb592965de24cf050d9
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-05-05 00:54:52 -0300

    Added more effects

--- collapsible-mobile-menu/src/js/app.js
+++ collapsible-mobile-menu/src/js/app.js
@@ -3,6 +3,7 @@
 	var sthCollapsibleMenu = (new SthCollapsibleMenu());
 		sthCollapsibleMenu.setButtonId("menu-button");
 		sthCollapsibleMenu.setMenuId("menu-panel");
+		sthCollapsibleMenu.setContentId("page-content");
 		sthCollapsibleMenu.init();
 		
 })();
--- collapsible-mobile-menu/src/js/components/collapsible-menu.js
+++ collapsible-mobile-menu/src/js/components/collapsible-menu.js
@@ -7,9 +7,11 @@
 
 	function SthCollapsibleMenu(buttonElemId, menuElemId, menuWidth)
 	{
+		var CLASS_MENU_OPEN = "open";
 		var self = this;
 		self._button;
 		self._menu;
+		self._content;
 
 		function setButtonId(elementId)
 		{
@@ -37,6 +39,19 @@
 			return self;
 		}
 
+		function setContentId(elementId)
+		{
+			self._content = document.getElementById(elementId);
+
+			if(!self._content || self._content.length < 1)
+			{
+				throw new Error("There is not any element available on DOM which uses the given content id.");
+				self._content = undefined;
+			}
+
+			return self;
+		}
+
 		function init(){
 			validateRequiredProperties();
 			createButtonElemListener();
@@ -49,6 +64,9 @@
 
 			if(!self._menu)
 				throw new Error("SthCollapsibleMenu needs an id which represents the menu element. Use #setMenuId(elementId) method.");
+
+			if(!self._content)
+				throw new Error("SthCollapsibleMenu needs an id which represents the content element. Use #setContentId(elementId) method.");
 		}
 
 		function createButtonElemListener()
@@ -60,12 +78,67 @@
 
 		function toggleMenuPanel()
 		{
-			console.log("Element clicked");
+			if(isMenuOpen()) closeMenuPanel();
+			else openMenuPanel();
+		}
+
+		function isMenuOpen()
+		{
+			var classes = self._menu.className.split(" ");
+			return classes.some(function(className){
+				return className == CLASS_MENU_OPEN;
+			});
+		}
+
+		function closeMenuPanel()
+		{
+			removeOpenClassesFromMenu();
+			moveContentToLeft();
+		}
+
+		function removeOpenClassesFromMenu()
+		{
+			var classes = self._menu.className.split(" ");
+			if(classes.length < 1) return true;
+
+			classes = classes.filter(function(className){
+				return className != CLASS_MENU_OPEN;
+			});
+
+			self._menu.className = classes.join(" ");
+		}
+
+		function moveContentToLeft()
+		{
+			self._content.style.marginLeft = 0;
+			self._content.style.marginRight = 0;
+			self._content.style.transform = "scale(1)";
+		}
+
+		function openMenuPanel()
+		{
+			addOpenClassesOnMenu();
+			moveContentToRight();
+		}
+
+		function addOpenClassesOnMenu()
+		{
+			var classes = self._menu.className.split(" ");
+			classes.push("open");
+			self._menu.className = classes.join(" ");
+		}
+
+		function moveContentToRight()
+		{
+			self._content.style.marginLeft = "200px";
+			self._content.style.marginRight = "-200px";
+			self._content.style.transform = "scale(0.8)";
 		}
 
 		return {
 			setButtonId: setButtonId,
 			setMenuId: setMenuId,
+			setContentId: setContentId,
 			init: init
 		};
 	}
